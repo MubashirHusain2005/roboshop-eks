32162daacc15a40ec769b679fa6c552cdf9317e3
MubashirHusain2005 <stokemubashir@gmail.com>
2026-03-01 14:53:00 +0000

    Terraform apply

--- terraform/modules/eks/access-entry.tf
+++ terraform/modules/eks/access-entry.tf
@@ -8,7 +8,7 @@ data "aws_iam_role" "github_oidc_role" {
 resource "aws_eks_access_entry" "github_role" {
   cluster_name      = var.cluster_name
   principal_arn     = data.aws_iam_role.github_oidc_role.arn
-  kubernetes_groups = ["system:masters"]
+  kubernetes_groups = ["dev:admins"]
   type              = "STANDARD"
 }
 
@@ -21,6 +21,28 @@ data "aws_iam_user" "terraform_user" {
 resource "aws_eks_access_entry" "terraform_user" {
   cluster_name      = var.cluster_name
   principal_arn     = data.aws_iam_user.terraform_user.arn
-  kubernetes_groups = ["system:masters"]  
+  kubernetes_groups = ["dev-admins"]  
   type              = "STANDARD"
+}
+
+###RBAC
+
+resource "kubectl_manifest" "rbac" {
+    yaml_body = <<EOF
+
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: github-admin-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: Group
+  name: dev-admins
+  apiGroup: rbac.authorization.k8s.io
+EOF
+
+depends_on = [aws_eks_cluster.eks_cluster]
 }
