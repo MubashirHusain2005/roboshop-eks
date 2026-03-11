df56999ab339ef88872f705630c80ff3e71901bd
MubashirHusain2005 <stokemubashir@gmail.com>
2026-03-11 14:09:54 +0000

    Remove access entries

--- terraform/modules/eks/access-entry.tf
+++ terraform/modules/eks/access-entry.tf
@@ -1,7 +1,7 @@
 # GitHub Actions OIDC Role
-data "aws_iam_role" "github_oidc_role" {
-  name = "github.to.aws.oidc"
-}
+#data "aws_iam_role" "github_oidc_role" {
+  #name = "github.to.aws.oidc"
+#}
 
 #resource "aws_eks_access_entry" "github_role" {
   #cluster_name      = aws_eks_cluster.eks_cluster.name
@@ -10,42 +10,42 @@ data "aws_iam_role" "github_oidc_role" {
   #type              = "STANDARD"
 #}
 
-resource "aws_eks_access_policy_association" "github_role_admin" {
-  cluster_name  = var.cluster_name
-  principal_arn = data.aws_iam_role.github_oidc_role.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#resource "aws_eks_access_policy_association" "github_role_admin" {
+  #cluster_name  = var.cluster_name
+ # principal_arn = data.aws_iam_role.github_oidc_role.arn
+  #policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
-  access_scope {
-    type = "cluster"
-  }
+  #access_scope {
+    #type = "cluster"
+ # }
 
-  depends_on = [aws_eks_access_entry.github_role]
-}
+  #depends_on = [aws_eks_access_entry.github_role]
+#}
 
 # Terraform IAM User
-data "aws_iam_user" "terraform_user" {
-  user_name = "terraform-test"
-}
+#data "aws_iam_user" "terraform_user" {
+  #user_name = "terraform-test"
+#}
 
-resource "aws_eks_access_entry" "terraform_user" {
-  cluster_name      = var.cluster_name
-  principal_arn     = data.aws_iam_user.terraform_user.arn
-  kubernetes_groups = ["dev-admins"]
-  type              = "STANDARD"
-}
+#resource "aws_eks_access_entry" "terraform_user" {
+  #cluster_name      = var.cluster_name
+  #principal_arn     = data.aws_iam_user.terraform_user.arn
+ # kubernetes_groups = ["dev-admins"]
+  #type              = "STANDARD"
+#}
 
 ###In production I would not want to give cluster admin policy
-resource "aws_eks_access_policy_association" "terraform_user_admin" {
-  cluster_name  = var.cluster_name
-  principal_arn = data.aws_iam_user.terraform_user.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#resource "aws_eks_access_policy_association" "terraform_user_admin" {
+  #cluster_name  = var.cluster_name
+  #principal_arn = data.aws_iam_user.terraform_user.arn
+  #policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
-  access_scope {
-    type = "cluster"
-  }
+  #access_scope {
+   # type = "cluster"
+  #}
 
-  depends_on = [aws_eks_access_entry.terraform_user]
-}
+  #depends_on = [aws_eks_access_entry.terraform_user]
+#}
 
 # RBAC
 resource "kubectl_manifest" "rbac" {
