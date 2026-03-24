a7ec8c156c35990a1d5765947ee089f03cad5d4b
MubashirHusain2005 <stokemubashir@gmail.com>
2026-03-24 21:00:49 +0000

    Remove rbac clusterrolebinding

--- terraform/modules/eks/access-entry.tf
+++ terraform/modules/eks/access-entry.tf
@@ -56,24 +56,24 @@ resource "aws_eks_access_policy_association" "terraform_user_admin" {
 
 
 # RBAC
-resource "kubectl_manifest" "rbac" {
-  yaml_body = <<EOF
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: dev-admin-binding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: Group
-  name: dev-admins
-  apiGroup: rbac.authorization.k8s.io
-EOF
+#resource "kubectl_manifest" "rbac" {
+  #yaml_body = <<EOF
+#apiVersion: rbac.authorization.k8s.io/v1
+#kind: ClusterRoleBinding
+#metadata:
+  #name: dev-admin-binding
+#roleRef:
+  #apiGroup: rbac.authorization.k8s.io
+  #kind: ClusterRole
+  #name: cluster-admin
+#subjects:
+#- kind: Group
+ # name: dev-admins
+ # apiGroup: rbac.authorization.k8s.io
+#EOF
 
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
+  #depends_on = [aws_eks_cluster.eks_cluster]
+#}
 
 
 
